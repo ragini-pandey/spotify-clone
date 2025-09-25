cfe1f6e3fc657422c1c836f6333f93ebb6082f56
Ragini Pandey <pandeyragini55@gmail.com>
2025-09-25 13:05:15 +0530

    redirect url dynamic

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -17,6 +17,6 @@ export async function middleware(req) {
 
   //Redirect them to login if they don't have token AND are requesting a protected route
   if (!token && pathname !== "/login") {
-    return NextResponse.redirect("http://localhost:3000/login");
+    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`);
   }
 }
